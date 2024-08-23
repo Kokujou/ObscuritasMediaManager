a58f2945272f38dd147e16af41d07d5be8bf31fc
Kokujou <Kokujou@gmx.de>
2024-08-23 12:59:29 +0200

    improve socket connection stability

--- Backend/Controllers/InteropProxyController.cs
+++ Backend/Controllers/InteropProxyController.cs
@@ -10,6 +10,18 @@ public class InteropProxyController(InteropConnectionChecker connectionChecker) 
     [HttpGet]
     public async Task ConnectToInteropAsync(CancellationToken token)
     {
+        if (connectionChecker.IsConnected) return;
+        try
+        {
+            using var httpClient = new HttpClient();
+            var result = await httpClient.GetAsync("http://localhost:8005", token);
+            return;
+        }
+        catch
+        {
+            // not connected
+        }
+
         while (!connectionChecker.IsConnected && !token.IsCancellationRequested) await Task.Yield();
     }
 }
